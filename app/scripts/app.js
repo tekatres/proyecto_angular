32b759699eabf6871756c2875cafefe3bb6357bf
Jesus <tekatres@gmail.com>
2016-10-13 15:58:09 +0200

    Punto de partida

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myappApp
+ * @description
+ * # myappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myappApp', ['ngAnimate','ngCookies','ngResource','ngRoute','ngSanitize','ngTouch'])
+
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myappApp
+ */
+angular.module('myappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
